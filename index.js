293f00be331b90300fc7b8ee8aa3535f3e92decd
mostafizurRahaman <fahim654326@gmail.com>
2022-12-29 00:42:51 +0600

    verifyJwt middle ware function created for verify jwt token

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const port = process.env.PORT || 5000;
 
 // jwt validation middleware : 
 
+const  verifyJWT = (req, res, next) => {
+   const authHeader = req.headers.authorization; 
+   if(!authHeader){
+      return res.status(401).send({message: "unauthorized access"})
+   }
+
+   const token = authHeader.split(' ')[1]; 
+   console.log(token); 
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded){
+      if(error){
+         return res.status(403).send({message: 'forbidden access'});
+      }
+      req.decoded = decoded; 
+      next(); 
+   })
+}
+
 // function verifyJWT(req, res, next){
 //    const authHeader = authorization.header; 
 // }
